1e791329d5ce7604df8dbd148aa1de7944c0a733
Fxcilities <48619853+Fxcilities@users.noreply.github.com>
2021-03-05 08:45:04 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shuana',
+    version='1.0.0',
+    lisence='MIT',
+    description='shuana is horny',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type='text/markdown',
+    author='fxcilities',
+    packages=find_packages(),
+    install_requires=['aiohttp'],                    
+)
